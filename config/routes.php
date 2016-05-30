8804f21034778c389a37652cba4539abaa2934ed
Guido Contreras Woda <gcontreras@digbang.com>
2016-05-30 14:22:08 -0300

    Changed local storage disk name

--- config/routes.php
+++ config/routes.php
@@ -2,7 +2,7 @@
 use Illuminate\Support\Facades\Storage;
 
 $binders = function(){
-    foreach (Storage::disk('app')->allFiles('Http/Routes') as $file)
+    foreach (Storage::disk('local')->allFiles('Http/Routes') as $file)
     {
 	    yield 'App\\' . str_replace(['/', '.php'], ['\\', ''], $file);
     }
